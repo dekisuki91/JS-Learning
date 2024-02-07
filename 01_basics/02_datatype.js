c741110a1328a84d7cdb7c0a7975847eb139077c
Rohit Singh <159244848+dekisuki91@users.noreply.github.com>
2024-02-07 13:21:34 +0000

    conversion done

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -2,7 +2,26 @@
 
 //alert(3+3) //we are using nodejs not browser.
 
-console.log(
-    +3); //control readability should be high.
+console.log(3
+    +
+    3); //control readability should be high.
 
 console.log("Rohit")
+
+let name="Hitesh"
+let age=20
+let isLoggedIn=false
+let undefined;
+
+//Number => 2 to power 53
+//Bigint 
+//String=""
+//boolean =>true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+console.log(typeof Undefined); // undefined
+console.log(typeof null); // Object 
+
+console.log([name,age,isLoggedIn]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// //let score = 33
+
+// console.log(typeof score); //numberr
+// console.log(typeof(score));//number
+
+let score = "33abc"
+console.log(typeof score); //string 
+console.log(typeof(score));//string
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);//Nan (not a number)
+
+
+
+
