ae7435636fcd867938c6d8e54495ea6039e60010
Rohit Singh <159244848+dekisuki91@users.noreply.github.com>
2024-02-07 11:40:10 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId =122234 
+let accountEmail = "rohit9161m@gmail.com"
+var accountPassword = "12345"
+accountcity = "Jaipur"
+let accountState = "Rajasthan"
+
+//we are not using var because the scope variable and functional variable.
+
+//accountId=2;
+accountEmail="aamil1234@.com"
+accountPassword="897234"
+accountcity="Deoria"
+accountState="U.P"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
